d8b05b2a46f959c5c3babd17f4c3b04dff325f44
Pranav Chopra <41725998+chopraji94@users.noreply.github.com>
2020-02-21 15:58:19 +0530

    Classes and objetcs

---
+++ Classes and Objects Assignments/Classes and Objects Assignments/C&O.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+namespace Classes_and_Objects_Assignments
+{
+    class StudentS
+    {
+        public String name { get; set; }
+        public int DateofBirth { get; set; }
+        public string Class { get; set; }
+        string Contact;
+        public string Mobile { get { return Contact; } set { Contact = "91" + value; } }
+        public string Section { get; set; }
+        public string School { get { return "chitkara"; } }
+
+        public StudentS()
+        {
+            name = "shivam";
+            DateofBirth = 10 / 10 / 1999;
+            Class = "10th";
+            Mobile = "+917807248671";
+            Section = "A";
+        }
+
+     
+    }
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            StudentS s = new StudentS();
+            System.Diagnostics.Debug.WriteLine("Name of Student is:" + s.name);
+
+            System.Diagnostics.Debug.WriteLine("DOB of Student is:" + s.DateofBirth);
+            System.Diagnostics.Debug.WriteLine("Class of Student is:" + s.Class);
+            System.Diagnostics.Debug.WriteLine("Mobile of Student is:" + s.Mobile);
+            System.Diagnostics.Debug.WriteLine("Section of Student is:" + s.Section);
+
+        }
+    }
+}
+
