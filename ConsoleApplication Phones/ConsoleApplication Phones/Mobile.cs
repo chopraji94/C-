7a442c84fb53fbfc483fe8ca98d119d625c5c415
Pranav Chopra <41725998+chopraji94@users.noreply.github.com>
2020-02-21 15:59:21 +0530

    Abstraction Only

---
+++ ConsoleApplication Phones/ConsoleApplication Phones/Mobile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_Phones
+{
+    
+        
+      public  abstract class feature
+        {
+            public abstract void calling();
+        }
+
+       public  class Apple : feature
+        {
+
+            public override void calling()
+            {
+                System.Diagnostics.Debug.WriteLine("Camera: 64MP");
+                System.Diagnostics.Debug.WriteLine("Storage 64gb ");
+                System.Diagnostics.Debug.WriteLine("Calling:Dual");
+            }
+        }
+        public class Samsung : feature
+        {
+            public override void calling()
+            {
+                System.Diagnostics.Debug.WriteLine("Camera:32MP");
+                System.Diagnostics.Debug.WriteLine("Storage:32gb");
+                System.Diagnostics.Debug.WriteLine("Calling:Dual Sim");
+            }
+
+             
+       
+        }
+        class Test
+        {
+            public static void Main(string[] args)
+            {
+             
+                string name = Console.ReadLine();
+                System.Diagnostics.Debug.WriteLine("Name of brand: " + name);
+                feature f;
+            name = name.ToLower();
+
+            if(name.Equals("apple"))
+            {
+                f = new Apple();
+                f.calling();
+            } else if (name.Equals("samsung") )
+            {
+                f = new Samsung();
+                f.calling();
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Brand not available");
+            }
+                   
+                    
+
+                
+                   
+                
+            }
+        }
+    }
