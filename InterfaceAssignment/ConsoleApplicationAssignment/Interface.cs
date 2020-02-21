3ea167c2c21e98c4040fa25e16fd8066bd84d267
Pranav Chopra <41725998+chopraji94@users.noreply.github.com>
2020-02-21 16:02:01 +0530

    Abstraction Only

---
+++ InterfaceAssignment/ConsoleApplicationAssignment/Interface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationAssignment
+{
+   public interface I1
+    {
+        void method();
+    }
+    public interface I2
+    {
+        void method();
+    }
+    class Interfaces
+    {
+        
+            void method()
+        {
+                System.Diagnostics.Debug.WriteLine("This is a console application");
+            }
+        static void Main(string[]args)
+        {
+            Interfaces I = new Interfaces();
+            I.method();
+                 
+        }
+        }
+    }
+
